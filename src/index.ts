5c6484393de60504579d21842bb324ba269c2ddb
nicki <nicky.stru@gmail.com>
2022-03-27 20:01:22 +0300

    tests working

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-import express from 'express';
+import express from "express";
+import 'dotenv/config'
+const app = express();
+app.use(express.json());
+app.listen(process.env.PORT_SERVER,()=>{
+    console.log(`Server is running on port ${process.env.PORT_SERVER}`);
+})
