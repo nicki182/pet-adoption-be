7dc6732d8366c6ae627668720a44a81ea74856e2
nicki <nicky.stru@gmail.com>
2022-03-27 20:19:15 +0300

    server starts to run

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import 'dotenv/config'
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 app.use(express.json());
-app.listen(process.env.PORT_SERVER,()=>{
-    console.log(`Server is running on port ${process.env.PORT_SERVER}`);
-})
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+});
