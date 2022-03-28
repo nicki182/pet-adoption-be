cadf3773f4cade7bbc1ae9bef5c58c27ad2740f1
nicki <nicky.stru@gmail.com>
2022-03-28 18:24:26 +0300

    show husky1

--- dist/utils/authentication.js
+++ dist/utils/authentication.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.verifyToken = exports.generateToken = void 0;
 const generateToken = (userId) => {
-    return jwt.sign({ userId }, process.env.JWT_KEY);
+  return jwt.sign({ userId }, process.env.JWT_KEY);
 };
 exports.generateToken = generateToken;
 const verifyToken = (token) => {
-    return jwt.verify(token, process.env.JWT_KEY);
+  return jwt.verify(token, process.env.JWT_KEY);
 };
 exports.verifyToken = verifyToken;
