cffaf78b1358ad1700523c1cb805f803ae3477fa
Nicole <31940739+nicki182@users.noreply.github.com>
2022-04-06 20:55:23 +0300

    Delete dist directory

--- dist/routes/session/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var router = (0, express_1.Router)();
-router.get("/", function (req, res) {
-  res.send("Hello World!");
-});
-//# sourceMappingURL=index.js.map
